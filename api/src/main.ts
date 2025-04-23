d30def22374928bfd526626176b8e01f5df7c72c
brownei <esitibrownson@gmail.com>
2025-04-23 22:20:45 +0100

    rerouting to monorepo

---
+++ api/src/main.ts
@@ -0,0 +1,15 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import * as dotenv from 'dotenv';
+import { Logger } from '@nestjs/common';
+
+async function bootstrap() {
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  dotenv.config();
+
+  await app.listen(process.env.PORT ?? 3000);
+  Logger.log(`Application is running on port: ${process.env.PORT}`)
+}
+bootstrap();
---
+++ web/src/routes/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { createFileRoute } from '@tanstack/react-router'
+
+export const Route = createFileRoute('/')({
+  component: App,
+})
+
+function App() {
+  return (
+    <div className="text-center">
+      <header className="min-h-screen flex flex-col items-center justify-center bg-[#282c34] text-white text-[calc(10px+2vmin)]">
+        Brownson
+      </header>
+    </div>
+  )
+}
